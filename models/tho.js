0d65d0a525416c59a1403db6c4e148f25d7964c2
THCN <thanhacun@gmail.com>
2017-11-07 16:59:49 +0700

    [ADD] sync local data with algolia index

--- models/tho.js
+++ models/tho.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongooseAlgolia = require('mongoose-algolia');
 
 const Tho = mongoose.Schema({
   index: {type: Number, unique: true},
@@ -6,5 +7,13 @@ const Tho = mongoose.Schema({
   content: String,
   footer: String
 });
+
+// for sync index with Algolia
+Tho.plugin(mongooseAlgolia, {
+  appId: '4VFRX3XOJ8',
+  apiKey: 'bf1aafb5802e23d47ce8111177f9d755',
+  indexName: 'dev_THO',
+  selector: 'index title content'
+})
 
 module.exports = mongoose.model('Tho', Tho);
