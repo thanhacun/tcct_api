8f953234057e888c166f708681efa8c9d21b1e9a
THCN <thanhacun@gmail.com>
2017-11-08 16:26:10 +0700

    [FIX] use only process.env for global variabbles

--- utils/auth_check.js
+++ utils/auth_check.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const configAuth = require('../config/auth');
 
 /* Auth checker middleware function */
 
@@ -13,7 +12,7 @@ module.exports = (req, res, next) => {
   // NOTE: no need the provider as it is always bearer
   const [provider, token] = req.headers.authorization.split(' ');
   // decode the token using the secret key phrase
-  return jwt.verify(token, configAuth.jwtSecret, (err, decoded) => {
+  return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {error.message = 'Cannot verify user!';}
 
     const decodedUser = decoded.sub;
