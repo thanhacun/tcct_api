0bbd86753e97299a15a0ae1dda8b29515273cf47
THCN <thanhacun@gmail.com>
2017-11-08 16:58:03 +0700

    [FIX] used static server not client-render option

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // production mode and for SPA (client-render)
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function(req, res){
+app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
