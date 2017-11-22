5ecfafb33cd4bd5c7e26a690355cddf89743f7ff
THCN <thanhacun@gmail.com>
2017-11-22 09:57:51 +0000

    [UPD] CORS in development mode
    [UPD] use only one start script

--- app.js
+++ app.js
@@ -45,12 +45,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-//enable CORS in express
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+//enable CORS in express for development
+if (process.env.CORS) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
 
 /* PASSPORT */
 // NOTE: jwt does not require session
